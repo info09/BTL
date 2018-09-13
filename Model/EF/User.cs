590e9be3f42febcaf05c6fd62431c8d293e73b61
Huy <huytq3103@gmail.com>
2018-09-13 21:45:26 +0700

    CRUD User phần Admin

--- Model/EF/User.cs
+++ Model/EF/User.cs
@@ -46,6 +46,6 @@ namespace Model.EF
         [StringLength(50)]
         public string ModifiedBy { get; set; }
 
-        public bool? Status { get; set; }
+        public bool Status { get; set; }
     }
 }
---
+++ OnlineShop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,91 @@
+﻿using Model.Dao;
+using Model.EF;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class UserController : BaseController
+    {
+        // GET: Admin/User
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 10)
+        {
+            var dao = new UserDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+
+                user.Password = Encryotor.MD5Hash(user.Password);
+                user.CreatedDate = DateTime.Now;
+                var id = dao.Insert(user);
+                if (id > 0)
+                {
+                    SetAlert("Update user thành công", "success");
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    SetAlert("Thêm user không thành công", "error");
+                    ModelState.AddModelError("", "Thêm user không thành công");
+                }
+
+            }
+            return View("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var user = new UserDao().ViewDetail(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                if (!string.IsNullOrEmpty(user.Password))
+                {
+                    var encryptMd5 = Encryotor.MD5Hash(user.Password);
+                    user.Password = encryptMd5;
+                }
+                var result = dao.Update(user);
+                if (result)
+                {
+                    SetAlert("Update user thành công", "success");
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật user không thành công");
+                }
+            }
+            return View("Index");
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new UserDao().Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
