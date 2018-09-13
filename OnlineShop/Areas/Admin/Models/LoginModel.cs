c2f50488ad35c96e039950240fd9cdb5afd0bbaf
Huy <huytq3103@gmail.com>
2018-09-13 16:07:28 +0700

    Add project files.

---
+++ OnlineShop/Areas/Admin/Models/LoginModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Areas.Admin.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Bạn phải nhập UserName")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Bạn phải nhập Password")]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
