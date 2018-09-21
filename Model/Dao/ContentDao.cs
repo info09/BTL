ac605671aaeeb5629353b58b351d01639fad9432
Huy <huytq3103@gmail.com>
2018-09-21 12:36:18 +0700

    add CRUD phần Content trong admin

--- Model/Dao/ContentDao.cs
+++ Model/Dao/ContentDao.cs
@@ -1,4 +1,5 @@
 ﻿using Model.EF;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,64 @@ namespace Model.Dao
         public Content GetById(long id)
         {
             return db.Contents.Find(id);
+        }
+
+        public IEnumerable<Content> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<Content> model = db.Contents.OrderByDescending(x => x.CreatedDate);
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Name.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+
+        public long Insert(Content content)
+        {
+            content.CreatedDate = DateTime.Now;
+            db.Contents.Add(content);
+            db.SaveChanges();
+            return content.ID;
+        }
+
+        public bool Update(Content entity)
+        {
+            try
+            {
+                var content = db.Contents.Find(entity.ID);
+                content.Name = entity.Name;
+                content.MetaTitle = entity.MetaTitle;
+                content.Image = entity.Image;
+                content.Description = entity.Description;
+                content.MetaDescription = entity.MetaDescription;
+                content.MetaKeyword = entity.MetaKeyword;
+                content.ModifiedDate = DateTime.Now;
+                content.CategoryID = entity.CategoryID;
+                content.Detail = entity.Detail;
+                content.Status = entity.Status;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var content = db.Contents.Find(id);
+                db.Contents.Remove(content);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
--- OnlineShop/Areas/Admin/Controllers/ContentController.cs
+++ OnlineShop/Areas/Admin/Controllers/ContentController.cs
@@ -8,14 +8,19 @@ using System.Web.Mvc;
 
 namespace OnlineShop.Areas.Admin.Controllers
 {
-    public class ContentController : Controller
+    public class ContentController : BaseController
     {
         // GET: Admin/Content
-        public ActionResult Index()
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
         {
-            return View();
+            var dao = new ContentDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
         }
 
+        [HttpGet]
+        [ValidateInput(false)]
         public ActionResult Create()
         {
             SetViewBag();
@@ -23,33 +28,66 @@ namespace OnlineShop.Areas.Admin.Controllers
         }
 
         [HttpPost]
+        [ValidateInput(false)]
         public ActionResult Create(Content model)
         {
             if (ModelState.IsValid)
             {
-
+                var dao = new ContentDao();
+                long id = dao.Insert(model);
+                if (id > 0)
+                {
+                    SetAlert("Thêm thông tin thành công", "success");
+                    return RedirectToAction("Index", "Content");
+                }
+                else
+                {
+                    SetAlert("Thêm user không thành công", "error");
+                    ModelState.AddModelError("", "Thêm thông tin không thành công");
+                }
             }
             SetViewBag();
             return View();
         }
 
+        [HttpGet]
+        [ValidateInput(false)]
         public ActionResult Edit(long id)
         {
             var dao = new ContentDao();
             var content = dao.GetById(id);
             SetViewBag(content.CategoryID);
-            return View();
+            return View(content);
         }
 
         [HttpPost]
+        [ValidateInput(false)]
         public ActionResult Edit(Content model)
         {
             if (ModelState.IsValid)
             {
+                var dao = new ContentDao();
 
+                var result = dao.Update(model);
+                if (result)
+                {
+                    SetAlert("Update content thành công", "success");
+                    return RedirectToAction("Index", "Content");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật content không thành công");
+                }
             }
             SetViewBag(model.CategoryID);
             return View();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new ContentDao().Delete(id);
+            return RedirectToAction("Index");
         }
 
         public void SetViewBag(long? selectedId = null)
