d0555e48f992ea070b20734276482575d4225e93
Huy <huytq3103@gmail.com>
2018-10-03 20:59:29 +0700

    add CRUD menu trong admin

--- Model/Dao/MenuDao.cs
+++ Model/Dao/MenuDao.cs
@@ -1,4 +1,5 @@
 ﻿using Model.EF;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,64 @@ namespace Model.Dao
         public List<Menu> ListByGroupId(int groupId)
         {
             return db.Menus.Where(x => x.TypeID == groupId && x.Status==true).OrderBy(x=>x.DisplayOrder).ToList();
+        }
+
+        public IEnumerable<Menu> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<Menu> model = db.Menus.OrderByDescending(x => x.Text);
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Text.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.Text).ToPagedList(page, pageSize);
+        }
+
+        public long Insert(Menu menu)
+        {
+            db.Menus.Add(menu);
+            db.SaveChanges();
+            return menu.ID;
+        }
+
+        public bool Update(Menu entity)
+        {
+            try
+            {
+                var menu = db.Menus.Find(entity.ID);
+                menu.Text = entity.Text;
+                menu.Link = entity.Link;
+                menu.DisplayOrder = entity.DisplayOrder;
+                menu.TypeID = entity.TypeID;
+                menu.Target = entity.Target;
+                
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var menu = db.Menus.Find(id);
+                db.Menus.Remove(menu);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public Menu GetById(long id)
+        {
+            return db.Menus.Find(id);
         }
     }
 }
---
+++ OnlineShop/Areas/Admin/Controllers/MenuController.cs
@@ -0,0 +1,89 @@
+﻿using Model.Dao;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class MenuController : BaseController
+    {
+        // GET: Admin/Menu
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
+        {
+            var dao = new MenuDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+
+        [HttpGet]
+        [ValidateInput(false)]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Menu model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new MenuDao();
+                long id = dao.Insert(model);
+                if (id > 0)
+                {
+                    SetAlert("Thêm thông tin thành công", "success");
+                    return RedirectToAction("Index", "Menu");
+                }
+                else
+                {
+                    SetAlert("Thêm user không thành công", "error");
+                    ModelState.AddModelError("", "Thêm thông tin không thành công");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [ValidateInput(false)]
+        public ActionResult Edit(long id)
+        {
+            var dao = new MenuDao();
+            var menu = dao.GetById(id);
+            return View(menu);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(Menu model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new MenuDao();
+
+                var result = dao.Update(model);
+                if (result)
+                {
+                    SetAlert("Update content thành công", "success");
+                    return RedirectToAction("Index", "Menu");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật content không thành công");
+                }
+            }
+            return View();
+        }
+        
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new MenuDao().Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
