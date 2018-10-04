36bb1aadfb3d5f01eb3b1e8fc303a4419f5c4d7f
Huy <huytq3103@gmail.com>
2018-10-04 14:59:41 +0700

    Quản lý about trong admin

---
+++ Model/Dao/AboutDao.cs
@@ -0,0 +1,80 @@
+﻿using Model.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Dao
+{
+    public class AboutDao
+    {
+        OnlineShopDbContext db = null;
+        public AboutDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+
+        public IEnumerable<About> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<About> model = db.Abouts.OrderByDescending(x => x.CreatedDate);
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Name.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+
+        public long Insert(About about)
+        {
+            about.CreatedDate = DateTime.Now;
+            db.Abouts.Add(about);
+            db.SaveChanges();
+            return about.ID;
+        }
+
+        public bool Update(About entity)
+        {
+            try
+            {
+                var about = db.Abouts.Find(entity.ID);
+                about.Name = entity.Name;
+                about.MetaTitle = entity.MetaTitle;
+                about.MetaDescription = entity.MetaDescription;
+                about.Image = entity.Image;
+                about.Detail = entity.Detail;
+                entity.ModifiedDate = DateTime.Now;
+                about.MetaKeywords = entity.MetaKeywords;
+                about.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var about = db.Abouts.Find(id);
+                db.Abouts.Remove(about);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public About GetById(long id)
+        {
+            return db.Abouts.Find(id);
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,93 @@
+﻿using Model.Dao;
+using Model.EF;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class AboutController : BaseController
+    {
+        // GET: Admin/About
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
+        {
+            var dao = new AboutDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+
+        [HttpGet]
+        [ValidateInput(false)]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(About model)
+        {
+            if (ModelState.IsValid)
+            {
+                var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+                model.CreatedBy = session.UserName;
+                var dao = new AboutDao();
+                long id = dao.Insert(model);
+                if (id > 0)
+                {
+                    SetAlert("Thêm thông tin thành công", "success");
+                    return RedirectToAction("Index", "About");
+                }
+                else
+                {
+                    SetAlert("Thêm user không thành công", "error");
+                    ModelState.AddModelError("", "Thêm thông tin không thành công");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [ValidateInput(false)]
+        public ActionResult Edit(long id)
+        {
+            var dao = new AboutDao();
+            var about = dao.GetById(id);
+            return View(about);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(About model)
+        {
+            if (ModelState.IsValid)
+            {
+                var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+                model.ModifiedBy = session.UserName;
+                var dao = new AboutDao();
+                var result = dao.Update(model);
+                if (result)
+                {
+                    SetAlert("Update content thành công", "success");
+                    return RedirectToAction("Index", "About");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật content không thành công");
+                }
+            }
+            return View();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new AboutDao().Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
