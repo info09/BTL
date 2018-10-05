87a44b5314fcd714e921491de05c11d741b4022e
Huy <huytq3103@gmail.com>
2018-10-05 10:39:40 +0700

    Add crud slide trong admin

--- Model/Dao/SlideDao.cs
+++ Model/Dao/SlideDao.cs
@@ -1,4 +1,5 @@
 ﻿using Model.EF;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,67 @@ namespace Model.Dao
         public List<Slide> ListAll()
         {
             return db.Slides.Where(x => x.Status == true).OrderBy(x => x.DisplayOrder).ToList();
+        }
+
+        public IEnumerable<Slide> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<Slide> model = db.Slides.OrderByDescending(x => x.CreatedDate);
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Image.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+
+        public long Insert(Slide slide)
+        {
+            slide.CreatedDate = DateTime.Now;
+            db.Slides.Add(slide);
+            db.SaveChanges();
+            return slide.ID;
+        }
+
+        public bool Update(Slide entity)
+        {
+            try
+            {
+                var slide = db.Slides.Find(entity.ID);
+                slide.Image = entity.Image;
+                slide.DisplayOrder = entity.DisplayOrder;
+                slide.Link = entity.Link;
+                slide.Description = entity.Description;
+                entity.ModifiedDate = DateTime.Now;
+                slide.MetaKeywords = entity.MetaKeywords;
+                slide.MetaDescription = entity.MetaDescription;
+                slide.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var slide = db.Slides.Find(id);
+                db.Slides.Remove(slide);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public Slide GetById(long id)
+        {
+            return db.Slides.Find(id);
         }
     }
 }
---
+++ OnlineShop/Areas/Admin/Controllers/SlideController.cs
@@ -0,0 +1,93 @@
+﻿using Model.Dao;
+using Model.EF;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class SlideController : BaseController
+    {
+        // GET: Admin/Slide
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
+        {
+            var dao = new SlideDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+
+        [HttpGet]
+        [ValidateInput(false)]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Slide model)
+        {
+            if (ModelState.IsValid)
+            {
+                var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+                model.CreatedBy = session.UserName;
+                var dao = new SlideDao();
+                long id = dao.Insert(model);
+                if (id > 0)
+                {
+                    SetAlert("Thêm thông tin thành công", "success");
+                    return RedirectToAction("Index", "Slide");
+                }
+                else
+                {
+                    SetAlert("Thêm slide không thành công", "error");
+                    ModelState.AddModelError("", "Thêm thông tin không thành công");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [ValidateInput(false)]
+        public ActionResult Edit(long id)
+        {
+            var dao = new SlideDao();
+            var slide = dao.GetById(id);
+            return View(slide);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(Slide model)
+        {
+            if (ModelState.IsValid)
+            {
+                var session = (UserLogin)Session[CommonConstants.USER_SESSION];
+                model.ModifiedBy = session.UserName;
+                var dao = new SlideDao();
+                var result = dao.Update(model);
+                if (result)
+                {
+                    SetAlert("Update slide thành công", "success");
+                    return RedirectToAction("Index", "Slide");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật slide không thành công");
+                }
+            }
+            return View();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new SlideDao().Delete(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
